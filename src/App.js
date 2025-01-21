521c6408e19a8f8fa1141a4a82195fd5a518f378
Harris Alam <harrisalam3@gmail.com>
2025-01-21 12:37:45 +0000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+/*import './App.css';*/
+
+function MyButton() {
+    return (
+        <button>I'm a button</button>
+    );
+}
+
+function PageHeader() {
+    return (
+        <h1>Welcome to my first ReactJS App</h1>
+    )
+}
+
+export default MyButton;
