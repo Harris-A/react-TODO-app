6d818328401def97ed20979ac9b2d1edba638e15
Harris Alam <harrisalam3@gmail.com>
2025-01-24 11:50:54 +0000

    using react variables, functions & components to add a navbar and header.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
-import logo from './logo.svg';
-/*import './App.css';*/
+import './App.css';
+import Navbar from './Navbar';
+import React from 'react';
+import { createRoot } from 'react-dom/client';
 
-function MyButton() {
-    return (
-        <button>I'm a button</button>
+// Main App component
+function App () {
+    const container = document.getElementById('root');
+    const root = createRoot(container);
+
+    const myList = (
+        <div>
+            <Navbar />
+            <header className="App-header">
+                <h1 className="page-header text-black text-5xl font-semibold tracking-tight text-balance sm:text-7xl">React TO-DO List</h1>
+            </header>
+            <ul className="text-black text-center m-8">
+                <li> Learn React</li>
+                <li> Create a basic application using React</li>
+                <li> Learn and use TailwindCSS</li>
+            </ul>
+        </div>
     );
+    root.render(myList);
 }
-
-function PageHeader() {
-    return (
-        <h1>Welcome to my first ReactJS App</h1>
-    )
-}
-
-export default MyButton;
+export default App; // Exporting the App component
---
+++ src/Navbar.js
@@ -0,0 +1,13 @@
+const Navbar = () => {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <h1>The DOJO Blog</h1>
+            <div className="links">
+             <a className="navbar-brand" href="#"></a>
+             <a className="navbar-brand" href="#">Home</a>
+             <a className="navbar-brand" href="#">About</a>
+            </div>
+        </nav>
+    );
+}
+export default Navbar;
