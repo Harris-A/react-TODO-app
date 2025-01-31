e9c02b528c93f1f06cb6d5656e53ca9de58418da
Harris Alam <harrisalam3@gmail.com>
2025-01-31 10:12:15 +0000

    add logo to navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,13 +1,10 @@
 const Navbar = () => {
     return (
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-        <h1>The DOJO Blog</h1>
-            <div className="links">
-             <a className="navbar-brand" href="#"></a>
-             <a className="navbar-brand" href="#">Home</a>
-             <a className="navbar-brand" href="#">About</a>
-            </div>
-        </nav>
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+                <div className="container">
+                    <a className="navbar-brand"><img src="/logo192.png" width="50"/></a>
+                </div>
+            </nav>
     );
 }
 export default Navbar;
