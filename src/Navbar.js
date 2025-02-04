7cc2a49fc914531e4f65735a407bff3a6d231be9
Harris Alam <harrisalam3@gmail.com>
2025-02-04 14:19:29 +0000

    Add darkmode toggle to navbar component

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,8 +1,11 @@
+import DarkModeToggle from "./DarkModeToggle"; // Import the toggle
+import React from "react";
 const Navbar = () => {
     return (
             <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
                 <div className="container">
                     <a className="navbar-brand"><img src="/logo192.png" width="50"/></a>
+                    <DarkModeToggle /> {/* Add the dark mode toggle button */}
                 </div>
             </nav>
     );
