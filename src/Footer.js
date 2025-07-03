1b44c92d7391c4304eddf77b757c7d55dc8d4557
harris alam <harrisalam3@gmail.com>
2025-07-03 14:09:31 +0100

    update footer text

--- src/Footer.js
+++ src/Footer.js
@@ -2,7 +2,7 @@ const Footer = () => {
     return (
         <footer className="bg-transparent text-gray-800 dark:text-white py-4 text-center mt-10 absolute bottom-0">
             <div className="container mx-auto px-4">
-                <p className="text-sm">&copy; {new Date().getFullYear()} Harris To-Do App. All rights reserved.</p>
+                <p className="text-sm">&copy; {new Date().getFullYear()} - Present. Harris To-Do App. All rights reserved.</p>
             </div>
         </footer>
     );
